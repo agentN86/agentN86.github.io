8c85cd3f118d416fdff0922ff0669beb65c5fab7
agentN86 <128328445+agentN86@users.noreply.github.com>
2024-05-23 16:27:50 -0500

    hi

--- agentsgams/scripts/gamHTML.js
+++ agentsgams/scripts/gamHTML.js
@@ -14,4 +14,11 @@ var template = `
 </div>
 `
 
-document.getElementById('gamList').innerHTML = document.getElementById('gamList').innerHTML + template
+document.getElementById('gamList').innerHTML = `
+<br>
+<div style="display: flex; align-items: top;">
+    <a name="${game}"></a>
+    <a href="${link}">&nbsp;&nbsp;<img src="${image}" alt="${game}" width="100" style="border: 3px solid ${color};"></a><span class="title">&nbsp;&nbsp;<a style="color: white; cursor: pointer;" href="${link}">${game_formatted}</a></span>
+    <p1>&nbsp;${description}</p1>
+</div>
+`
