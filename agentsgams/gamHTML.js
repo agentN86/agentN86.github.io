408f411120ff94e064c7fb9cf8e749772b865ae5
agentN86 <128328445+agentN86@users.noreply.github.com>
2024-05-23 16:36:52 -0500

    it works, but it doesnt work?

--- agentsgams/gamHTML.js
+++ agentsgams/gamHTML.js
@@ -14,11 +14,4 @@ var template = `
 </div>
 `
 
-document.getElementById('gamList').innerHTML = `
-<br>
-<div style="display: flex; align-items: top;">
-    <a name="${game}"></a>
-    <a href="${link}">&nbsp;&nbsp;<img src="${image}" alt="${game}" width="100" style="border: 3px solid ${color};"></a><span class="title">&nbsp;&nbsp;<a style="color: white; cursor: pointer;" href="${link}">${game_formatted}</a></span>
-    <p1>&nbsp;${description}</p1>
-</div>
-`
+document.getElementById('gamList').innerHTML = document.getElementById('gamList').innerHTML + template
